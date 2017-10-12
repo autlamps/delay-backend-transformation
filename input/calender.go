fd1146bd004b5551cf9cc3c35f79fb485c5fc054
dominicbporter <dominic@dbp.kiwi>
2017-10-12 23:43:29 +1300

    Added verisoning for all AT GTFS spec data
    + Updating all AT GTFS when a new verison is released
    - verison.go due to not being needed
    Need to remove old verisons which are not being used, and also verison control the rest of the database

--- input/calender.go
+++ input/calender.go
@@ -11,12 +11,19 @@ import (
 func (is *InService) CaIn(entities update.CAEntities) {
 
 	tx, err := is.Db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	udte, err := tx.Prepare("UPDATE calendar SET gtfs_service_id = $1, monday = $2, tuesday = $3, wednesday = $4, thursday = $5, friday = $6, saturday = $7, sunday = $8 WHERE service_id = $9")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	stmt, err := tx.Prepare(pq.CopyIn("calendar", "gtfs_service_id", "service_id", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for i := 0; i < len(entities); i++ {
 		gtfs_service_id := entities[i].ServiceID
@@ -27,19 +34,37 @@ func (is *InService) CaIn(entities update.CAEntities) {
 		friday := boolcheck(entities[i].Friday)
 		saturday := boolcheck(entities[i].Saturday)
 		sunday := boolcheck(entities[i].Sunday)
-		service_id, err := uuid.NewRandom()
-		if err != nil {
-			log.Fatal(err)
-		}
-		is.ServiceMap[gtfs_service_id] = service_id
 
-		_, err = stmt.Exec(gtfs_service_id, service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
-		if err != nil {
-			log.Fatal(err)
+		itemuuid := is.GTFSServiceMap[is.removeVersion(entities[i].ServiceID)]
+
+		if itemuuid != uuid.Nil {
+			_, err = udte.Exec(gtfs_service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, itemuuid)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+
+			service_id, err := uuid.NewRandom()
+			if err != nil {
+				log.Fatal(err)
+			}
+			is.ServiceMap[gtfs_service_id] = service_id
+			gtfsID, itemuuid := is.toGTFSMap(is.ServiceMap, gtfs_service_id)
+			is.GTFSServiceMap[gtfsID] = itemuuid
+
+			_, err = stmt.Exec(gtfs_service_id, service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 
 	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = udte.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
--- input/route.go
+++ input/route.go
@@ -15,6 +15,11 @@ func (is *InService) RoIn(entities update.ROEntities) {
 		log.Fatal(err)
 	}
 
+	udte, err := tx.Prepare("UPDATE routes SET gtfs_route_id = $1 WHERE route_id = $2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	stmt, err := tx.Prepare(pq.CopyIn("routes", "gtfs_route_id", "route_id", "agency_id", "route_short_name", "route_long_name", "route_type"))
 	if err != nil {
 		log.Fatal(err)
@@ -28,23 +33,34 @@ func (is *InService) RoIn(entities update.ROEntities) {
 		route_type := entities[i].RouteType
 		agency_id := is.AgencyMap[gtfs_agency_id]
 
-		route_id, err := uuid.NewRandom()
+		itemuuid := is.GTFSRouteMap[is.removeVersion(entities[i].RouteID)]
+		//Checking if the route on any GTFS version has a uuid already
+		if itemuuid != uuid.Nil {
+			udte.Exec(gtfs_route_id, itemuuid)
+		} else {
 
-		if err != nil {
-			log.Fatal(err)
-		}
+			route_id, err := uuid.NewRandom()
 
-		is.RouteMap[gtfs_route_id] = route_id
-		gtfsID, iteamuuid := is.toGTFSMap(is.RouteMap, gtfs_route_id)
-		is.GTFSRouteMap[gtfsID] = iteamuuid
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		_, err = stmt.Exec(gtfs_route_id, route_id, agency_id, route_short_name, route_long_name, route_type)
-		if err != nil {
-			log.Fatal(err)
+			is.RouteMap[gtfs_route_id] = route_id
+			gtfsID, iteamuuid := is.toGTFSMap(is.RouteMap, gtfs_route_id)
+			is.GTFSRouteMap[gtfsID] = iteamuuid
+
+			_, err = stmt.Exec(gtfs_route_id, route_id, agency_id, route_short_name, route_long_name, route_type)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
-
 	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = udte.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
